b13ed90cda853242bc08921892812b49e1a0bb85
MajdiFirman <majdif.hafsyah@gmail.com>
2024-03-06 19:10:01 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
+  <!-- font familly -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+
+  <!-- logo tittlebar -->
+  <link rel="icon" href="assets/logo.png" type="image/c-icon">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="style.css">
+
+  <title>HotelBook</title>
+</head>
+<body>
+  
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg bg-transparant">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="assets/logo.png" alt="logo" width="120"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item mx-2">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Promo</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Akomodasi Pilihan</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Cari Hotel</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Bantuan</a>
+        </li>
+      </ul>
+      <div>
+        <button class="button-primary">Daftar</button>
+        <button class="button-secondary">Masuk</button>
+      </div>
+    </div>
+  </div>
+</nav>
+
+  <!-- bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
