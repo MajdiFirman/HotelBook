4ef1e44a3f349f4fc6b8f18e13d8abe16a5b7753
MajdiFirman <majdif.hafsyah@gmail.com>
2024-03-07 12:28:32 +0800

    update promo

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
   <link rel="icon" href="assets/logo.png" type="image/c-icon">
 
   <!-- CSS -->
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/style.css">
 
   <title>HotelBook</title>
 </head>
@@ -54,6 +54,14 @@
     </div>
   </div>
 </nav>
+
+<!-- Promo -->
+<div class="promo">
+<h3 class="fw-bold">Promo dan Diskon</h3>
+  <div class="prodis">
+  <a class="promo1" href=""><img src="assets/prodis1.png"class="img-fluid" alt=""></a>
+  </div>
+</div>
 
   <!-- bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
